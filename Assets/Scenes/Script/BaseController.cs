9dbbddc7c8dfa8a63e5dcd627ff4b8fac87c7f50
Olgaellik <olgaellik@gmail.com>
2019-02-03 01:50:40 +0300

    all in one

---
+++ Assets/Scenes/Script/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour 
+
+{
+	public bool IsEnabled { get; private set; }
+
+	public virtual void On()
+	{
+		IsEnabled = true;
+	}
+	public virtual void Off()
+		
+	{
+		IsEnabled = false;
+	}
+}
---
+++ Assets/Scenes/Script/Main.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyController;
+
+namespace MyController
+{
+	/// <summary>
+	/// Точка входа в программу
+	/// </summary>
+	
+	public sealed class Main : MonoBehaviour
+	{
+		public static Main Instance
+		{
+			get { return _instance; }
+		}
+
+		private GameObject _controllersGameObject;
+		private InputController _inputController;
+		private TorchController _torchController;
+		private static Main _instance;
+
+		void Start()
+		{
+			_instance = this;
+			_controllersGameObject = new GameObject {name = "Controllers"};
+			_inputController = _controllersGameObject.AddComponent<InputController>();
+			_torchController = _controllersGameObject.AddComponent<TorchController>();
+		}
+
+		#region Property      
+
+		/// <summary>
+		/// Получить контроллер фонарика
+		/// </summary>
+
+		public TorchController GetTorchController
+		{
+			get { return _torchController; }
+		}
+
+		/// <summary>
+		/// Получить контроллер ввода данных
+		/// </summary>
+		/// <returns></returns>
+
+		public InputController GetInputController()
+		{
+			return _inputController;
+		}
+
+		#endregion
+
+
+		
+
+	}
+}
