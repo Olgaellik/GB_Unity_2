758a3dce7dcd39ad20651180270380ff31a553e3
Olgaellik <olgaellik@gmail.com>
2019-02-16 16:33:02 +0300

    custom spawn\despawn editor

---
+++ Assets/Lesson5/Editor/MyEditor.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MyEditor : EditorWindow
+{
+    public GameObject Obj1;
+    public GameObject Obj2;
+    public GameObject Obj3;
+    string _nameObject = "Hello World";
+    bool groupEnabled;
+    bool _randomColor = true;
+    int _countObject = 1;
+    float _radius = 10;
+
+    public Object source;
+
+    Color[] _colors = new Color[]
+    {
+        Color.green, Color.black, Color.blue, Color.clear, Color.cyan, Color.red, Color.yellow, Color.white,
+        Color.red
+    };
+
+    [MenuItem("MyEditor/Items")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(MyEditor));
+    }
+
+    int _choiceIndex = 0;
+
+    private Vector3 _minPos, _maxPos;
+    private List<GameObject> _objs = new List<GameObject>();
+    void OnGUI()
+    {
+        GUILayout.Label("Настройки", EditorStyles.boldLabel);
+
+        //Obj1 = EditorGUILayout.Popup("Выбери объект",Obj1);
+        //_choiceIndex = EditorGUILayout.Popup("Выбери объект",_choiceIndex, _choices);
+        //if (_choiceIndex < 0)
+        //	_choiceIndex = 0;
+
+        Obj1 =
+            EditorGUILayout.ObjectField("Объект 1", Obj1, typeof(GameObject), true) as GameObject;
+        Obj2 =
+            EditorGUILayout.ObjectField("Объект 2", Obj2, typeof(GameObject), true) as GameObject;
+        Obj3 =
+            EditorGUILayout.ObjectField("Объект 3", Obj3, typeof(GameObject), true) as GameObject;
+
+        _minPos = EditorGUILayout.Vector3Field("Min Pos", _minPos);
+        _maxPos = EditorGUILayout.Vector3Field("Max Pos", _maxPos);
+        
+        var objs = new List<GameObject>();
+        if (Obj1!=null)
+            objs.Add(Obj1);
+        if (Obj2!=null)
+            objs.Add(Obj2);
+        if (Obj3!=null)
+            objs.Add(Obj3);
+        
+        
+        _nameObject = EditorGUILayout.TextField("Имя объекта", _nameObject);
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Дополнительные настройки", groupEnabled);
+        _randomColor = EditorGUILayout.Toggle("Случайный цвет", _randomColor);
+        _countObject = EditorGUILayout.IntSlider("Количество объектов", _countObject, 1, 100);
+        //_radius = EditorGUILayout.Slider("Радиус окружности", _radius, 10, 50);
+        EditorGUILayout.EndToggleGroup();
+        GUI.backgroundColor = Color.green;
+        if (GUILayout.Button("Создать объекты"))
+        {
+            Spawn(objs);
+        }
+                
+        GUI.backgroundColor = Color.red;
+        if (GUILayout.Button("Уничтожить новые объекты"))
+        {
+            RemoveSpawn();
+        }
+        if (GUILayout.Button("Уничтожить все объекты"))
+        {
+            RemoveAllSpawn();
+        }
+    }
+
+    void RemoveSpawn()
+    {
+        for (var index = _objs.Count - 1; index >= 0; index--)
+        {
+            var gameObject = _objs[index];
+            DestroyImmediate(gameObject);
+        }
+    }
+    
+    void RemoveAllSpawn()
+    {
+        var objs = GameObject.FindGameObjectsWithTag("EditorObjects");
+        for (var index = objs.Length - 1; index >= 0; index--)
+        {
+            var gameObject = objs[index];
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void Spawn(List<GameObject> objs)
+    {
+        if (objs.Count <= 0)
+        {
+            Debug.LogError("No objects to spawn");
+            return;
+        }
+        _objs.Clear();
+        
+        GameObject root = new GameObject("EditorObjects");
+        root.tag = "EditorObjects";
+        for (int i = 0; i < _countObject; i++)
+        {
+            //float angle = i * Mathf.PI * 2 / _countObject;
+            //Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * _radius;
+            var pos = new Vector3(
+                Random.Range(_minPos.x, _maxPos.x),
+                Random.Range(_minPos.y, _maxPos.y),
+                Random.Range(_minPos.z, _maxPos.z));
+
+            GameObject temp = Instantiate(objs[Random.Range(0, objs.Count)], pos, Quaternion.identity);
+            temp.name = _nameObject + "(" + i + ")";
+            temp.transform.parent = root.transform;
+
+            var r = temp.GetComponentInChildren<Renderer>();
+            _objs.Add(temp);
+
+            if (r == null || !_randomColor) continue;
+            
+            var tempMaterial = new Material(r.sharedMaterial);
+            tempMaterial.color = _colors[Random.Range(0, _colors.Length - 1)];
+            r.sharedMaterial = tempMaterial;
+
+            //temp.GetComponent<Renderer>().material.color = _colors[Random.Range(0, _colors.Length - 1)];
+        }
+    }
+}
